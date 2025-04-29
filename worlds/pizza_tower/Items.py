8c92e0968db01cd75cb7a0157d8458867c0b81d6
unsafetyskizzers <camden44mason@gmail.com>
2025-04-29 09:41:29 -0500

    __init__: writing methods

--- worlds/pizza_tower/Items.py
+++ worlds/pizza_tower/Items.py
@@ -8,12 +8,10 @@ class PTItemData(NamedTuple):
     code: typing.Optional[int]
     classification: any
 
-item_progression = {
+pt_items = {
     "Toppin": PTItemData(101, ItemClassification.progression_skip_balancing),
     "Boss Key": PTItemData(102, ItemClassification.progression),
-}
 
-item_moveset = {
     "Mach 4": PTItemData(103, ItemClassification.useful),
     "Uppercut": PTItemData(104, ItemClassification.progression),
     "Superjump": PTItemData(105, ItemClassification.progression),
@@ -23,32 +21,24 @@ item_moveset = {
     "Supertaunt": PTItemData(109, ItemClassification.useful),
     "Bodyslam": PTItemData(110, ItemClassification.progression),
     "Breakdance": PTItemData(111, ItemClassification.filler),
-}
 
-item_moveset_peppino = {
     "Peppino: Wallclimb": PTItemData(112, ItemClassification.progression),
     "Peppino: Dive": PTItemData(113, ItemClassification.progression),
     "Gustavo & Brick: Double Jump": PTItemData(114, ItemClassification.progression),
     "Gustavo & Brick: Rat Kick": PTItemData(115, ItemClassification.progression),
     "Gustavo & Brick: Walljump": PTItemData(116, ItemClassification.progression),
     "Gustavo: Spin Attack": PTItemData(117, ItemClassification.progression),
-}
 
-item_moveset_noise = {
     "Noise: Wallbounce": PTItemData(118, ItemClassification.progression),
     "Noise: Tornado": PTItemData(119, ItemClassification.useful),
     "Noise: Crusher": PTItemData(120, ItemClassification.progression),
-}
 
-item_trap = {
     "Clown Trap": PTItemData(121, ItemClassification.trap),
     "One-Minute Timer": PTItemData(122, ItemClassification.trap),
     "Pizzaface Trap": PTItemData(123, ItemClassification.trap),
     "Slow Trap": PTItemData(124, ItemClassification.trap),
     "Oktoberfest!": PTItemData(125, ItemClassification.trap),
-}
 
-item_other = {
     "Tower Secret Treasure": PTItemData(126, ItemClassification.filler),
     "Cross Buff": PTItemData(127, ItemClassification.filler),
     "Pizza Shield": PTItemData(128, ItemClassification.filler),
--- worlds/pizza_tower/__init__.py
+++ worlds/pizza_tower/__init__.py
@@ -1,8 +1,10 @@
 from worlds.AutoWorld import World, WebWorld, Region
-from .Items import PTItem, item_list
+from BaseClasses import MultiWorld
+from .Items import PTItem, pt_items
 from .Locations import PTLocation, pt_locations
 from .Options import PTOptions
 from .Regions import create_regions
+from .Rules import set_rules
 import typing
 
 class PizzaTowerWebWorld(WebWorld):
@@ -16,7 +18,43 @@ class PizzaTowerWorld(World):
     options_dataclass = PTOptions
     options: PTOptions
 
-    def create_items(self):
-        pizzaitempool = []
+    def create_item(self, item: str) -> PTItem:
+        return PTItem(item, pt_items[item][1], pt_items[item][0], self.player)
 
-        self.multiworld.itempool += pizzaitempool
+    def create_regions(self):
+        create_regions(self.player, self.multiworld)
+
+    def create_items(self):
+        pizza_itempool = []
+
+        locations_to_fill = 152
+
+        #shared moves
+        pizza_itempool.append(self.create_item("Mach 4"))
+        pizza_itempool.append(self.create_item("Uppercut"))
+        pizza_itempool.append(self.create_item("Superjump"))
+        pizza_itempool.append(self.create_item("Grab"))
+        pizza_itempool.append(self.create_item("Taunt"))
+        pizza_itempool.append(self.create_item("Supertaunt"))
+        pizza_itempool.append(self.create_item("Bodyslam"))
+        pizza_itempool.append(self.create_item("Breakdance"))
+
+        #peppino's moves (and gus + brick)
+        pizza_itempool.append(self.create_item("Peppino: Wallclimb"))
+        pizza_itempool.append(self.create_item("Peppino: Dive"))
+        pizza_itempool.append(self.create_item("Gustavo & Brick: Double Jump"))
+        pizza_itempool.append(self.create_item("Gustavo & Brick: Rat Kick"))
+        pizza_itempool.append(self.create_item("Gustavo & Brick: Walljump"))
+        pizza_itempool.append(self.create_item("Gustavo: Spin Attack"))
+
+        #add keys and toppins
+        for i in range(4): pizza_itempool.append(self.create_item("Boss Key"))
+        for i in range(95): pizza_itempool.append(self.create_item("Toppin"))
+
+        #fill the rest of the pool with points. get around to putting more stuff in the pool later
+        for i in range(locations_to_fill - len(pizza_itempool)): pizza_itempool.append(self.create_item("1,000 Points"))
+
+        self.multiworld.itempool += pizza_itempool
+
+    def set_rules(self):
+        set_rules(self, self.multiworld)
